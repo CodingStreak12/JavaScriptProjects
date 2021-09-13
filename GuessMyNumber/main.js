c166e9b3fbcb3931a0a336fecd9755dd57545013
CodeStreak12 <codestreak12@Karanjeets-MacBook-Air.local>
2021-09-13 16:51:40 -0400

    Completed: 1. Logic of the game. 2. Winning And losing. 3. Score meter. 4. Random Number. 5. Winning Text. Left with: 1. Resetting the button. 2. High Score.

--- GuessMyNumber/main.js
+++ GuessMyNumber/main.js
@@ -1,8 +1,41 @@
 "use strict";
 const number = document.querySelector(".number");
-const check = docuement.querySelector(".check");
+const check = document.querySelector(".check");
 const again = document.querySelector(".again");
 const guess = document.querySelector(".guess");
-const message = docuement.querySelector(".message");
-const score = docuement.querySelector(".score");
+const message = document.querySelector(".message");
+const score = document.querySelector(".score");
 const highScore = document.querySelector(".highScore");
+
+const ULTIMATE_NUMBER = Number.parseInt(Math.random() * 20 + 1).toString();
+let highScoreValue = 20;
+let scoreValue = 20;
+
+function correctAnser() {
+  document.querySelector("body").style.backgroundColor = "Green";
+  message.textContent = "🎉 Congratulations You Won";
+  number.textContent = ULTIMATE_NUMBER;
+}
+function wrongAnswer() {
+  scoreValue -= 1;
+  score.textContent = scoreValue;
+}
+check.addEventListener("click", () => {
+  if (guess.value === ULTIMATE_NUMBER) {
+    correctAnser();
+  } else {
+    if (guess.value > ULTIMATE_NUMBER) {
+      message.textContent = "Guess Lower";
+      wrongAnswer();
+    } else if (guess.value < ULTIMATE_NUMBER) {
+      message.textContent = "Guess Higher";
+      wrongAnswer();
+    }
+  }
+});
+again.addEventListener("click", () => {
+  scoreValue = 20;
+  score.textContent = scoreValue;
+  number.textContent = "?";
+  n;
+});
