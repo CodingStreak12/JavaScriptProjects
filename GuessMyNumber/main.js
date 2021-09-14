27aa36aade58034121480c1440697602aa2e7568
CodingStreak12 <nitinww53@gmail.com>
2021-09-14 02:55:22 -0400

    Completed: 1. Everything completed. Completed the High Score as well. Left: 1. Refractoring of code left only.

--- GuessMyNumber/main.js
+++ GuessMyNumber/main.js
@@ -7,7 +7,7 @@ const message = document.querySelector(".message");
 const score = document.querySelector(".score");
 const highScore = document.querySelector(".highScore");
 
-let ULTIMATE_NUMBER = Number.parseInt(Math.random() * 20 + 1).toString();
+let ULTIMATE_NUMBER = Number.parseInt(Math.random() * 20 + 1);
 let highScoreValue = 0;
 let scoreValue = 20;
 again.addEventListener("click", () => {
@@ -17,27 +17,32 @@ again.addEventListener("click", () => {
   scoreValue = 20;
   score.textContent = scoreValue;
   number.textContent = "?";
-  ULTIMATE_NUMBER = Number.parseInt(Math.random() * 20 + 1).toString();
-  console.log(ULTIMATE_NUMBER);
+  ULTIMATE_NUMBER = Number.parseInt(Math.random() * 20 + 1);
 });
 
 function correctAnser() {
   document.querySelector("body").style.backgroundColor = "Green";
   message.textContent = "🎉 Congratulations You Won";
   number.textContent = ULTIMATE_NUMBER;
+  if (scoreValue > highScoreValue) {
+    highScoreValue = scoreValue;
+    highScore.textContent = highScoreValue;
+  }
 }
 function wrongAnswer() {
   scoreValue -= 1;
   score.textContent = scoreValue;
 }
+
 check.addEventListener("click", () => {
-  if (guess.value === ULTIMATE_NUMBER) {
+  if (Number.parseInt(guess.value) === ULTIMATE_NUMBER) {
     correctAnser();
+    return;
   } else {
-    if (guess.value > ULTIMATE_NUMBER) {
+    if (Number.parseInt(guess.value) > ULTIMATE_NUMBER) {
       message.textContent = "Guess Lower";
       wrongAnswer();
-    } else if (guess.value < ULTIMATE_NUMBER) {
+    } else if (Number.parseInt(guess.value) < ULTIMATE_NUMBER) {
       message.textContent = "Guess Higher";
       wrongAnswer();
     }
